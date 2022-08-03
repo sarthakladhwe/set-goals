8dfd2771269f11bb79475d2f1b6481ab30f114cd
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-08-03 23:10:24 +0530

    Removed routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,17 +7,4 @@ router.get('/', (req, res) => {
     })
 })
 
-router.put('/', (req, res) => {
-    res.status(200).json({
-        message: "Get Goals"
-    })
-})
-
-router.push('/', (req, res) => {
-    res.status(200).json({
-        message: "Get Goals"
-    })
-    
-})
-
 module.exports = router 
