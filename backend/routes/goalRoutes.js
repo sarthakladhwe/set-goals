3b6ac24da71212c29d097ea03471511a57aa91d2
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-08-03 23:27:38 +0530

    POST, PUT, DELETE

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,4 +7,22 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    res.status(200).json({
+        message: "Set Goal"
+    })
+})
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({
+        message: `Update Goal ${req.params.id}`
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({
+        message: `Delete Goal ${req.params.id}`
+    })
+})
+
 module.exports = router 
