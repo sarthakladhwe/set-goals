c482301ff7ac7d3a5837d132af758f33492fa64d
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-08-05 23:24:23 +0530

    Controller file

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,12 @@
+//@desc Get Goals
+//@route GET /api/goals/
+//@access Private
+const getGoals = (req, res) => {
+    res.status(200).json({
+        message: "Get Goals"
+    })
+}
+
+module.exports = {
+    getGoals
+}
