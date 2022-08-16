4bc1e2a763b370d1e7491dfa24029192552058ca
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-08-16 23:10:58 +0530

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
-const app = express()
+connectDB()
+
+const app = express()   //Create instance of the express
 
 app.use(express.json())
 app.use(express.urlencoded({
