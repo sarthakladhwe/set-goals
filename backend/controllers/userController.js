696da6b30dfe7c1e8d8fceee8ebc3c36a5a05914
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-08-22 23:23:42 +0530

    Delete User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,4 +2,8 @@ const registerUser = (req, res) => {
     res.json({message: "Register User"})
 }
 
+const deleteuser = (req, res) => {
+    res.json({message: "Delete User"})
+}
+
 module.exports = registerUser
