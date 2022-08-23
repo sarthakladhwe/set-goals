968a9caecae47cc5af25dc91855fa83c88fc6243
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-08-23 23:28:00 +0530

    All user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,26 @@
+//@desc Register New User
+//@route POST /api/users/
+//@access Public
 const registerUser = (req, res) => {
     res.json({message: "Register User"})
 }
 
-const deleteuser = (req, res) => {
-    res.json({message: "Delete User"})
+//@desc Authentic User
+//@route POST /api/users/login
+//@access Public
+const loginUser = (req, res) => {
+    res.json({message: "Login User"})
 }
 
-module.exports = registerUser
+//@desc Get User Data
+//@route GET /api/users/me
+//@access Public
+const getMe = (req, res) => {
+    res.json({message: "User Data"})
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe
+}
